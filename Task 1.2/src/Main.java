b8b42ae0af9deb70c0e185d662ba86578e8bd12c
Aleksey <freem4n382@yandex.ru>
2022-12-30 01:26:54 +0300

    Task** ready

--- Task 1.2/src/Main.java
+++ Task 1.2/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
         int itCount = 0;
-        while(itCount <= 100) {
-            itCount+=2;
+        while (itCount <= 100) {
+            itCount += 2;
             System.out.println("Уволен айтишник с id " + itCount);
         }
     }
