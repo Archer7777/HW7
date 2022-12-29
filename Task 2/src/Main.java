23c8553307d522792c61a85c4ee9e8c83cf91edf
Aleksey <freem4n382@yandex.ru>
2022-12-30 01:17:57 +0300

    Task 2 ready. All twos IT mens os fired)))

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        for (int itCount = 0; itCount < 100; itCount+=2) {
+            if (itCount == 0) {
+                continue;
+            }
+            System.out.println("Уволен айтишник с id " + itCount);
+        }
     }
 }
